fix bug in evaluation resulting in no normalization when evaluating on CPU

evaluation/predict.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 import clearml_ids
 from dataset.grazpedwri_dataset import GrazPedWriDataModule
 from model.ao_classifier import AOClassifier
+from model.linear_evaluation import LinearEvaluation
 
 save_dir = Path("evaluation/predictions")
 
@@ -33,12 +34,18 @@ for experiment, clearml_id in experiment_dict.items():
     predictions = {}
 
     ckpt_path = Task.get_task(clearml_id).artifacts['best.ckpt'].get()
-    model = AOClassifier.load_from_checkpoint(ckpt_path, strict=False)
+    # choose correct model
+    if experiment.startswith("LE"):
+        model = LinearEvaluation.load_from_checkpoint(ckpt_path, strict=False)
+    else:
+        model = AOClassifier.load_from_checkpoint(ckpt_path, strict=False)
     model.eval()
 
     with torch.inference_mode():
         for batch in tqdm(data.test_dataloader(), desc=f"Save predictions for {experiment}"):
-            y_hat = model(batch).sigmoid()
+            if not torch.cuda.is_available():
+                batch['image'] = data.normalize(batch['image'])
+            y_hat = model(batch).sigmoid().cpu()
             predictions.update({file_name: y for file_name, y in zip(batch['file_name'], y_hat)})
 
     torch.save(predictions, save_dir / f"{experiment}_{clearml_id}.pt")

evaluation/quantitative.py
@@ -4,6 +4,8 @@ from dataset.grazpedwri_dataset import GrazPedWriDataset
 import pandas as pd
 from pathlib import Path
 
+mode = ['end2end', 'lin_eval'][0]
+
 metrics_kwargs = {'num_labels': GrazPedWriDataset.N_CLASSES, 'average': None}
 metrics = MetricCollection({
     "Acc@1": classification.MultilabelAccuracy(**metrics_kwargs),
@@ -18,7 +20,9 @@ gt = torch.load(pred_dir / 'ground_truth.pt')
 mean_df = pd.DataFrame(columns=['Experiment', 'Acc@1', 'F1', 'Precision', 'Recall', 'AUROC'])
 experiment_df = pd.DataFrame(columns=['Experiment', 'Acc@1', 'F1', 'Precision', 'Recall', 'AUROC', 'AO_Class'])
 for experiment in pred_dir.iterdir():
-    if experiment.stem == 'ground_truth' or experiment.is_dir():
+    is_line_eval = experiment.stem.startswith('LE')
+    match_mode = (mode == 'lin_eval' and is_line_eval) or (mode == 'end2end' and not is_line_eval)
+    if experiment.stem == 'ground_truth' or experiment.is_dir() or not match_mode:
         continue
 
     pred = torch.load(experiment)